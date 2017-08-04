fdbcd718cdbbf56bb45898d93166e3be0e7ffd31
mcflugen <mcflugen@gmail.com>
2017-08-03 21:50:59 -0600

    Add build for Mac and Linux.

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,36 +1,63 @@
 #! /bin/bash
 
-./configure \
-  --with-proj \
-  --with-proj-includes=$PREFIX/include \
-  --with-proj-libs=$PREFIX/lib \
-  --with-proj-share=$PREFIX/share \
-  --with-tiff-includes=$PREFIX/include \
-  --with-tiff-libs=$PREFIX/lib \
+export PATH=/usr/bin:/bin:/usr/sbin:/etc:/usr/lib:$PREFIX/bin
+
+export CC=$(which gcc)
+export CXX=$(which g++)
+
+# export CFLAGS="-fPIC $CFLAGS"
+# export CXXFLAGS="-fPIC $CXXFLAGS"
+
+CONFIGURE_FLAGS= \
+  --prefix=$PREFIX \
   --with-freetype \
-  --with-freetype-includes="$PREFIX/include/freetype2 $PREFIX/include" \
+  --with-freetype-includes=$PREFIX/include/freetype2 \
   --with-freetype-libs=$PREFIX/lib \
   --with-gdal=$PREFIX/bin/gdal-config \
+  --with-gdal-libs=$PREFIX/lib \
+  --with-proj=$PREFIX/bin/proj \
+  --with-proj-includes=$PREFIX/include \
+  --with-proj-libs=$PREFIX/lib \
+  --with-proj-share=$PREFIX/share/proj \
   --with-geos=$PREFIX/bin/geos-config \
-  --with-cairo \
+  --with-jpeg-includes=$PREFIX/include \
+  --with-jpeg-libs=/$PREFIX/lib \
+  --with-png-includes=$PREFIX/include \
+  --with-png-libs=$PREFIX/lib \
+  --with-tiff-includes=$PREFIX/include \
+  --with-tiff-libs=$PREFIX/lib \
   --without-postgres \
   --without-mysql \
   --with-sqlite \
-  --with-x \
+  --with-sqlite-libs=$PREFIX/lib \
+  --with-sqlite-includes=$PREFIX/include \
+  --with-fftw-includes=$PREFIX/include \
+  --with-fftw-libs=$PREFIX/lib \
   --with-cxx \
-  --with-opengl=aqua \
   --without-readline \
-  --prefix=$PREFIX \
-  --enable-macosx-app \
-  --with-python \
-  --with-wxwidgets=$PREFIX/bin/wx-config \
+  --enable-64bit \
+  --with-cairo \
+  --with-cairo-includes=$PREFIX/include/cairo \
+  --with-cairo-libs=$PREFIX/lib \
+  --with-cairo-ldflags="-lcairo" \
   --with-libs=$PREFIX/lib \
-  --with-includes=$PREFIX/include \
-  --with-nls
+  --with-includes=$PREFIX/include
+
+if [ $(uname) == Darwin ]; then
+  CONFIGURE_FLAGS=\
+    $CONFIGURE_FLAGS \
+    --with-opengl=aqua \
+    --enable-macosx-app \
+    --with-opencl
+#  --with-macosx-sdk=/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk
+fi
+
+./configure $CONFIGURE_FLAGS
 
 # --with-macosx-sdk=/Developer/SDKs/MacOSX10.8.sdk
 # --with-liblas="/Users/cmbarton/grass\_source/LAS/lasdist/bin/liblas-config"
 # --with-opencl
 
 make GDAL_DYNAMIC=
-make bindist
+
+# make bindist
